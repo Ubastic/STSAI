a55c9d44b65f1058e94dd3a14ac314e8a88ea7c2
Ryan Xu <ryanxu123@gmail.com>
2021-01-13 17:49:50 -0500

    Add basic AI

--- src/main/java/SlayTheSpireAIMod/AIs/MapAI.java
+++ src/main/java/SlayTheSpireAIMod/AIs/MapAI.java
@@ -1,6 +1,38 @@
 package SlayTheSpireAIMod.AIs;
 
+import SlayTheSpireAIMod.communicationmod.ChoiceScreenUtils;
+import com.megacrit.cardcrawl.map.MapRoomNode;
+
+import java.util.ArrayList;
+
 /** Class which decides what room to travel to. */
 public class MapAI {
+    /** Always take the leftmost node. */
+    public static void execute(){
+        if(ChoiceScreenUtils.getCurrentChoiceType() != ChoiceScreenUtils.ChoiceType.MAP) return;
+        ArrayList<String> choices = ChoiceScreenUtils.getCurrentChoiceList();
+        ChoiceScreenUtils.makeMapChoice(0);
+    }
+
+    private static class Path{
+        int events;
+        int shops;
+        int chests;
+        int rests;
+        int monsters;
+        int elites;
+        enum startType {EVENT, SHOP, CHEST, REST, MONSTER, ELITE}
+
+        /** Path starting from node root. */
+        public Path(MapRoomNode root){
+            int events = 0;
+            int shops = 0;
+            int chests = 0;
+            int rests = 0;
+            int monsters = 0;
+            int elites = 0;
+        }
+
+    }
 
 }
