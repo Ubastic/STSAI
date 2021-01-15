c238c06ef88d28691dfc2e7065e6fa58245b7c6a
Ryan Xu <ryanxu123@gmail.com>
2021-01-15 18:53:18 -0500

    Add fight-specific AI

--- src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/GremlinNobCAI.java
+++ src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/GremlinNobCAI.java
@@ -1,6 +1,11 @@
 package SlayTheSpireAIMod.AIs.CombatAIs;
 
+import SlayTheSpireAIMod.util.CombatUtils;
 import SlayTheSpireAIMod.util.Move;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import java.util.ArrayList;
 
 /** AI versus encounter "Gremlin Nob". */
 public class GremlinNobCAI extends AbstractCAI {
@@ -12,6 +17,22 @@ public class GremlinNobCAI extends AbstractCAI {
 
     @Override
     public Move pickMove() {
-        return genericPickMove();
+        ArrayList<AbstractMonster> monsters = AbstractDungeon.getCurrRoom().monsters.monsters;
+        CombatUtils.MonsterAttack[] monsterAttacks = new CombatUtils.MonsterAttack[monsters.size()];
+        int aliveMonsters = 0;
+        for(int i = 0; i < monsters.size(); i++){
+            AbstractMonster m = monsters.get(i);
+            if(m.currentHealth > 0){
+                aliveMonsters++;
+            }
+            monsterAttacks[i] = new CombatUtils.MonsterAttack(m);
+        }
+
+        // if player can kill only alive monster, do it
+        if(aliveMonsters == 1){
+            Move tryKill = toKill(CombatUtils.getRandomTarget());
+            if(tryKill != null) return tryKill;
+        }
+        return withFullBlockCard();
     }
 }
