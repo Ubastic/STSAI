f189c2dc60de9c32c0c55aeee0d3eb65d8a5d34f
Ryan Xu <ryanxu123@gmail.com>
2021-01-14 23:24:04 -0500

    Add trivial AI based on grid type

--- src/main/java/SlayTheSpireAIMod/AIs/GridSelectAI.java
+++ src/main/java/SlayTheSpireAIMod/AIs/GridSelectAI.java
@@ -1,15 +1,41 @@
 package SlayTheSpireAIMod.AIs;
 
 import SlayTheSpireAIMod.communicationmod.ChoiceScreenUtils;
+import SlayTheSpireAIMod.util.ScreenUpdateUtils;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.screens.select.GridCardSelectScreen;
 
 import java.util.ArrayList;
 
 /** Class which decides what to do at a grid choice. */
 public class GridSelectAI {
-    /** Select the first option. */
+    /** If a confirm screen is up, select confirm. Otherwise select and confirm an option. */
     public static void execute(){
         if(ChoiceScreenUtils.getCurrentChoiceType() != ChoiceScreenUtils.ChoiceType.GRID) return;
         ArrayList<String> choices = ChoiceScreenUtils.getCurrentChoiceList();
-        ChoiceScreenUtils.makeGridScreenChoice(0);
+
+        GridCardSelectScreen screen = AbstractDungeon.gridSelectScreen;
+        if(screen.confirmScreenUp){
+            ChoiceScreenUtils.pressConfirmButton();
+            return;
+        }
+        if(screen.forUpgrade){
+            // upgrade the last card
+            ChoiceScreenUtils.makeGridScreenChoice(choices.size() - 1);
+            ScreenUpdateUtils.update();
+            ChoiceScreenUtils.pressConfirmButton();
+        }else if(screen.forTransform){
+            // transform the first card
+            ChoiceScreenUtils.makeGridScreenChoice(0);
+            ScreenUpdateUtils.update();
+            ChoiceScreenUtils.pressConfirmButton();
+        }else if(screen.forPurge){
+            // purge the first card
+            ChoiceScreenUtils.makeGridScreenChoice(0);
+            ScreenUpdateUtils.update();
+            ChoiceScreenUtils.pressConfirmButton();
+        }else{ // AFAIK Only possibility is for duplicator
+
+        }
     }
 }
