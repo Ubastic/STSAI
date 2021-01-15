ccb8d4415679c8f46cb3f5d75960cbe369421193
Ryan Xu <ryanxu123@gmail.com>
2021-01-14 23:22:52 -0500

    Update method

--- src/main/java/SlayTheSpireAIMod/util/ScreenUpdateUtils.java
+++ src/main/java/SlayTheSpireAIMod/util/ScreenUpdateUtils.java
@@ -4,25 +4,29 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.input.InputHelper;
 import com.megacrit.cardcrawl.ui.buttons.PeekButton;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
+import com.megacrit.cardcrawl.vfx.PlayerTurnEffect;
+
+import java.util.Iterator;
 
 public class ScreenUpdateUtils {
     /** Update the current screen. If a screen-loading/closing action has been made, load/close that screen. */
     public static void update(){
         AbstractDungeon.CurrentScreen screen = AbstractDungeon.screen;
         switch(screen){
-//            case NO_INTERACT:
+            case NO_INTERACT:
             case NONE:
                 AbstractDungeon.dungeonMapScreen.update();
                 AbstractDungeon.currMapNode.room.update();
                 AbstractDungeon.scene.update();
                 AbstractDungeon.currMapNode.room.eventControllerInput();
                 break;
-//            case FTUE:
-//                ftue.update();
-//                InputHelper.justClickedRight = false;
-//                InputHelper.justClickedLeft = false;
-//                currMapNode.room.update();
-//                break;
+            case FTUE:
+                AbstractDungeon.ftue.update();
+                InputHelper.justClickedRight = false;
+                InputHelper.justClickedLeft = false;
+                AbstractDungeon.currMapNode.room.update();
+                break;
 //            case MASTER_DECK_VIEW:
 //                deckViewScreen.update();
 //                break;
@@ -44,12 +48,12 @@ public class ScreenUpdateUtils {
 //            case MAP:
 //                dungeonMapScreen.update();
 //                break;
-//            case GRID:
-//                gridSelectScreen.update();
-//                if (PeekButton.isPeeking) {
-//                    currMapNode.room.update();
-//                }
-//                break;
+            case GRID:
+                AbstractDungeon.gridSelectScreen.update();
+                if (PeekButton.isPeeking) {
+                    AbstractDungeon.currMapNode.room.update();
+                }
+                break;
             case CARD_REWARD:
                 AbstractDungeon.cardRewardScreen.update();
                 if (PeekButton.isPeeking) {
@@ -88,6 +92,47 @@ public class ScreenUpdateUtils {
 //            case DOOR_UNLOCK:
 //                CardCrawlGame.mainMenuScreen.doorUnlockScreen.update();
 //                break;
+        }
+        AbstractDungeon.turnPhaseEffectActive = false;
+        Iterator i = AbstractDungeon.topLevelEffects.iterator();
+
+        AbstractGameEffect e;
+        while(i.hasNext()) {
+            e = (AbstractGameEffect)i.next();
+            e.update();
+            if (e.isDone) {
+                i.remove();
+            }
+        }
+
+        i = AbstractDungeon.effectList.iterator();
+
+        while(i.hasNext()) {
+            e = (AbstractGameEffect)i.next();
+            e.update();
+            if (e instanceof PlayerTurnEffect) {
+                AbstractDungeon.turnPhaseEffectActive = true;
+            }
+
+            if (e.isDone) {
+                i.remove();
+            }
+        }
+
+        i = AbstractDungeon.effectsQueue.iterator();
+
+        while(i.hasNext()) {
+            e = (AbstractGameEffect)i.next();
+            AbstractDungeon.effectList.add(e);
+            i.remove();
+        }
+
+        i = AbstractDungeon.topLevelEffectsQueue.iterator();
+
+        while(i.hasNext()) {
+            e = (AbstractGameEffect)i.next();
+            AbstractDungeon.topLevelEffects.add(e);
+            i.remove();
         }
         AbstractDungeon.overlayMenu.update();
     }
