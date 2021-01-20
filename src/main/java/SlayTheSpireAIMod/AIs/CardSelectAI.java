113366e2bab683966fe48efb3c547813e67de381
Ryan Xu <ryanxu123@gmail.com>
2021-01-20 17:25:39 -0500

    Add basic card selection AI

--- src/main/java/SlayTheSpireAIMod/AIs/CardSelectAI.java
+++ src/main/java/SlayTheSpireAIMod/AIs/CardSelectAI.java
@@ -2,8 +2,11 @@ package SlayTheSpireAIMod.AIs;
 
 import SlayTheSpireAIMod.communicationmod.ChoiceScreenUtils;
 import basemod.DevConsole;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /** Class which decides what to do given card rewards. */
 public class CardSelectAI {
@@ -12,10 +15,14 @@ public class CardSelectAI {
         if(ChoiceScreenUtils.getCurrentChoiceType() != ChoiceScreenUtils.ChoiceType.CARD_REWARD){
             return;
         }
-        ArrayList<String> choices = ChoiceScreenUtils.getCurrentChoiceList();
-        if(choices.size() > 0){
-            ChoiceScreenUtils.makeCardRewardChoice(0);
+        ArrayList<String> choices = ChoiceScreenUtils.getCurrentChoiceList(); // can include "bowl"
+        ArrayList<AbstractCard> rewardCards = AbstractDungeon.cardRewardScreen.rewardGroup;
+        Deck deck = new Deck(AbstractDungeon.player.masterDeck);
+        String choice = deck.chooseCard(rewardCards);
+        if(choice != null){
+            ChoiceScreenUtils.makeCardRewardChoice(choices.indexOf(choice));
+        }else{
+            ChoiceScreenUtils.pressCancelButton();
         }
     }
-
 }
