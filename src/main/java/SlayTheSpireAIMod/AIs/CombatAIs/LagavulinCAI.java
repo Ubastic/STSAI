610796e5f56167e4c9af2cff35e80d9543ebca4b
Ryan Xu <ryanxu123@gmail.com>
2021-03-01 10:27:21 -0500

    Add fight-specific AI

--- src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/LagavulinCAI.java
+++ src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/LagavulinCAI.java
@@ -1,6 +1,11 @@
 package SlayTheSpireAIMod.AIs.CombatAIs;
 
+import SlayTheSpireAIMod.util.CombatUtils;
 import SlayTheSpireAIMod.util.Move;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+import java.util.ArrayList;
 
 /** AI versus encounter "Lagavulin". */
 public class LagavulinCAI extends AbstractCAI {
@@ -9,8 +14,34 @@ public class LagavulinCAI extends AbstractCAI {
         return "Lagavulin";
     }
 
+    // TODO play powers during dormant turns
     @Override
     public Move pickMove() {
-        return newGenericPickMove();
+        // if a no-negative card can be played, play it
+        Move tryFree = FreeCard();
+        if(tryFree != null){
+            return tryFree;
+        }
+
+        CardSequence start = new CardSequence();
+        ArrayList<AbstractCard> unplayable = new ArrayList<>();
+        for(AbstractCard c : start.simplePlayer.hand){
+            if(!c.canUse(AbstractDungeon.player, CombatUtils.getWeakestTarget())){
+                unplayable.add(c);
+            }
+        }
+        for(AbstractCard c : unplayable){
+            start.simplePlayer.hand.remove(c);
+        }
+
+        CardSequence bestState = start.getBestPossibility(x -> heuristic(x, 13));
+
+        if(bestState != start){
+            logger.info("Evaluated best state (from LagavulinCAI): " + bestState.toString());
+            int bestIndex = AbstractDungeon.player.hand.group.indexOf(bestState.first);
+            return new Move(Move.TYPE.CARD, bestIndex,
+                    AbstractDungeon.getCurrRoom().monsters.monsters.get(bestState.firstTargetIndex));
+        }
+        return new Move(Move.TYPE.PASS);
     }
 }
