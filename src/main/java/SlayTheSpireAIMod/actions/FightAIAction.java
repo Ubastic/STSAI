36f0e4c6faac0a2d67ef20b920516982bc47563e
Ryan Xu <ryanxu123@gmail.com>
2021-03-17 17:39:18 -0400

    Add ability to discard potion

--- src/main/java/SlayTheSpireAIMod/actions/FightAIAction.java
+++ src/main/java/SlayTheSpireAIMod/actions/FightAIAction.java
@@ -7,6 +7,8 @@ import com.megacrit.cardcrawl.actions.utility.NewQueueCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
+import com.megacrit.cardcrawl.potions.PotionSlot;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -26,28 +28,60 @@ public class FightAIAction extends AbstractGameAction {
         }
         ArrayList<AbstractCard> cards = AbstractDungeon.player.hand.group;
         Move toMake = CombatUtils.pickMove(); // There is no check for validity of non-card moves
+        logger.info("Making Move: " + toMake);
         switch (toMake.type){
             case CARD:
                 AbstractCard toPlay = cards.get(toMake.index);
-                if(!toPlay.canUse(AbstractDungeon.player, toMake.target)){
+                if(toPlay == null || !toPlay.canUse(AbstractDungeon.player, toMake.target)){
                     isDone = true;
-                    logger.info("Illegal card played: " + toMake.toString());
+                    logger.info("ERROR Illegal card on target played: " + toMake.toString());
                     return;
                 }
+                logger.info("Playing card: " + toPlay.name);
                 NewQueueCardAction queueCard = new NewQueueCardAction(cards.get(toMake.index), toMake.target);
                 this.addToTop(queueCard);
                 isDone = true;
                 break;
             case POTION:
-                //TODO test this after the AI starts to use potions
                 AbstractPotion toUse = AbstractDungeon.player.potions.get(toMake.index);
+                if(toUse == null || toUse instanceof PotionSlot){
+                    logger.info("ERROR Tried to use non-existent potion");
+                    isDone = true;
+                    break;
+                }
+                if(!toUse.canUse()){
+                    logger.info("ERROR Tried to use unusable potion: " + toMake.toString());
+                    isDone = true;
+                    break;
+                }
+                logger.info("Using potion: " + toUse.name);
                 toUse.use(toMake.target);
+                for (AbstractRelic r : AbstractDungeon.player.relics) {
+                    r.onUsePotion();
+                }
+                AbstractDungeon.topPanel.destroyPotion(toMake.index);
                 isDone = true;
                 break;
             case PASS:
                 isDone = true;
                 logger.info("Ending turn");
                 AbstractDungeon.overlayMenu.endTurnButton.disable(true);
+                break;
+            case DISCARD:
+                AbstractPotion toDiscard = AbstractDungeon.player.potions.get(toMake.index);
+                if(toDiscard == null || toDiscard instanceof PotionSlot){
+                    logger.info("ERROR Tried to discard non-existent potion");
+                    isDone = true;
+                    break;
+                }
+                if(!toDiscard.canDiscard()){
+                    logger.info("ERROR Tried to discard undiscardable potion: " + toMake.toString());
+                    isDone = true;
+                    break;
+                }
+                logger.info("Discarding potion: " + toDiscard.name);
+                AbstractDungeon.topPanel.destroyPotion(toDiscard.slot);
+                isDone = true;
                 break;
         }
     }
--- src/main/java/SlayTheSpireAIMod/util/Move.java
+++ src/main/java/SlayTheSpireAIMod/util/Move.java
@@ -7,10 +7,10 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
  *  - Play card
  *  - Use potion
  *  - Pass
- *  - TODO discard potion
+ *  - Discard potion
  * */
 public class Move {
-    public enum TYPE{CARD, POTION, PASS}
+    public enum TYPE{CARD, POTION, PASS, DISCARD}
     public TYPE type;
     public int index; // index of the card/potion to be used
     public AbstractMonster target;
@@ -21,17 +21,30 @@ public class Move {
         this.target = target;
     }
 
+    public Move(TYPE t, int index){
+        this(t, index, null);
+        if(t != TYPE.PASS && t != TYPE.DISCARD) throw new IllegalArgumentException("Not enough Move information");
+    }
+
     public Move(TYPE t){
         this(t, -1, null);
         if(t != TYPE.PASS) throw new IllegalArgumentException("Not enough Move information.");
     }
 
+
     @Override
     public String toString() {
+        if(target == null){
+            return "Move{" +
+                    "type=" + type +
+                    ", index=" + index +
+                    ", target=" + "None" +
+                    '}';
+        }
         return "Move{" +
                 "type=" + type +
                 ", index=" + index +
-                ", target=" + target +
+                ", target=" + target.name +
                 '}';
     }
 }
