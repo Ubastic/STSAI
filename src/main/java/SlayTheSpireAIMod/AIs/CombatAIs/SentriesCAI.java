871ba425952bfd3138d2360c619d5f7c4177cd60
Ryan Xu <ryanxu123@gmail.com>
2021-03-21 12:52:44 -0400

    Remove duplicate code

--- src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/SentriesCAI.java
+++ src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/SentriesCAI.java
@@ -2,12 +2,8 @@ package SlayTheSpireAIMod.AIs.CombatAIs;
 
 import SlayTheSpireAIMod.util.CombatUtils;
 import SlayTheSpireAIMod.util.Move;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.ArrayList;
 
 public class SentriesCAI extends AbstractCAI {
     public static final Logger logger = LogManager.getLogger(SentriesCAI.class.getName());
@@ -29,40 +25,13 @@ public class SentriesCAI extends AbstractCAI {
      * @return how good the state is
      * */
     public static int heuristic(CardSequence state, int tolerance){
+        int genericFactor = GenericCAI.heuristic(state, tolerance);
         int aliveMonsters = 0;
-        int totalHealth = 0;
-        int incomingDmg = 0;
-
-        int extraBlock = 0;
-        extraBlock += state.simplePlayer.metallicize;
-
-        if(AbstractDungeon.player.hasPower("Plated Armor")){
-            extraBlock += AbstractDungeon.player.getPower("Plated Armor").amount;
-        }
-
         for(CombatUtils.SimpleMonster m : state.simpleMonsters){
             if(m.isAlive()){
                 aliveMonsters += 1;
-                totalHealth += m.health;
-                incomingDmg += m.attack.getHitDamage(); // always only 1 hit
             }
         }
-
-        int willLoseHP = Math.max(0, incomingDmg - state.simplePlayer.block - extraBlock);
-        int hpLossFactor =  3 * Math.max(0, willLoseHP - tolerance);
-        int aliveMonstersFactor = 5 * aliveMonsters;
-
-        if(aliveMonsters == 0){
-            aliveMonstersFactor = -100;
-        }
-
-        int strength = state.simplePlayer.strength;
-        int strengthA = -5;
-        int strengthFactor = strength * strengthA;
-
-        int metallicize = state.simplePlayer.metallicize;
-        int metallicizeA = -3;
-        int metallicizeFactor = metallicize * metallicizeA;
 
         // it is better to kill either the left or right sentry first
         int multiSentryFactor = 0;
@@ -71,8 +40,6 @@ public class SentriesCAI extends AbstractCAI {
             CombatUtils.SimpleMonster right = state.simpleMonsters.get(2);
             multiSentryFactor = Math.min(left.health, right.health);
         }
-
-        return totalHealth + aliveMonstersFactor + hpLossFactor + strengthFactor + metallicizeFactor +
-                multiSentryFactor;
+        return genericFactor + multiSentryFactor;
     }
 }
