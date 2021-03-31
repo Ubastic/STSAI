f3a158d1838799ec4a9bf025f6f39509b1fbaa6e
Ryan Xu <rx26@cornell.edu>
2021-03-31 17:12:13 -0400

    Add documentation

--- src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/PotionEval.java
+++ src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/PotionEval.java
@@ -3,5 +3,12 @@ package SlayTheSpireAIMod.AIs.CombatAIs;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
 
 interface PotionEval {
+    /**
+     * Returns an evaluation of the usage of a potion.
+     *
+     * @param p the potion to evaluate
+     * @return  the evaluation of how good it is to use a potion. The larger the better.
+     *          0 indicates not useful enough to use.
+     * */
     int evaluation(AbstractPotion p);
 }
