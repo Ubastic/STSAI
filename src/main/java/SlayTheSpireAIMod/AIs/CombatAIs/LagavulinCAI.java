44f9c7b07e9ba0bc1ab20128e4722aaa3db4a43b
Ryan Xu <rx26@cornell.edu>
2021-04-01 23:18:25 -0400

    Utilize dormant turns

--- src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/LagavulinCAI.java
+++ src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/LagavulinCAI.java
@@ -2,7 +2,12 @@ package SlayTheSpireAIMod.AIs.CombatAIs;
 
 import SlayTheSpireAIMod.util.CombatUtils;
 import SlayTheSpireAIMod.util.Move;
+import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.red.DemonForm;
+import com.megacrit.cardcrawl.cards.red.Inflame;
+import com.megacrit.cardcrawl.cards.red.Metallicize;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,9 +23,79 @@ public class LagavulinCAI extends AbstractCAI {
         return "Lagavulin";
     }
 
-    // TODO play powers during dormant turns
     @Override
     public Move pickMove() {
+        int turn = GameActionManager.turn;
+        if(turn < 3){
+            Move tryPower = power();
+            if(tryPower != null){ return tryPower; }
+
+            boolean wait = false;
+            ArrayList<AbstractCard> draw = AbstractDungeon.player.drawPile.group;
+            for(AbstractCard c : draw){
+                if(c.name.equals("Bash+")){
+                    wait = true;
+                    break;
+                }
+                if(c.type == AbstractCard.CardType.POWER){
+                    wait = true;
+                    break;
+                }
+            }
+            if(wait){
+                return new Move(Move.TYPE.PASS);
+            }
+        }
         return GenericCAI.pickMove(x -> GenericCAI.heuristic(x, 1));
+    }
+
+    /**
+     * Returns a Move which plays a power card. Returns null if none exists.
+     * Uses decreasing priority: Demon Form, Inflame, Metallicize.
+     *
+     * @return a Move which plays a power card. Null if none exists
+     * */
+    public Move power(){
+        AbstractPlayer p = AbstractDungeon.player;
+        ArrayList<AbstractCard> hand = p.hand.group;
+        AbstractCard demonForm = null;
+        AbstractCard inflame = null;
+        AbstractCard metallicize = null;
+        AbstractCard otherPower = null;
+        for(AbstractCard c : hand){
+            if(c.type == AbstractCard.CardType.POWER) {
+                switch (c.cardID) {
+                    case DemonForm.ID:
+                        demonForm = c;
+                        break;
+                    case Inflame.ID:
+                        inflame = c;
+                        break;
+                    case Metallicize.ID:
+                        metallicize = c;
+                        break;
+                    default:
+                        otherPower = c;
+                }
+            }
+        }
+        if(demonForm != null){
+            if(demonForm.canUse(p, CombatUtils.getWeakestTarget())){
+                return new Move(Move.TYPE.CARD, hand.indexOf(demonForm), CombatUtils.getWeakestTarget());
+            }
+        }else if(inflame != null){
+            if(inflame.canUse(p, CombatUtils.getWeakestTarget())){
+                return new Move(Move.TYPE.CARD, hand.indexOf(inflame), CombatUtils.getWeakestTarget());
+            }
+        }else if(metallicize != null){
+            if(metallicize.canUse(p, CombatUtils.getWeakestTarget())){
+                return new Move(Move.TYPE.CARD, hand.indexOf(metallicize), CombatUtils.getWeakestTarget());
+            }
+        }else if(otherPower != null){
+            if(otherPower.canUse(p, CombatUtils.getWeakestTarget())){
+                return new Move(Move.TYPE.CARD, hand.indexOf(otherPower), CombatUtils.getWeakestTarget());
+            }
+        }
+        return null;
     }
 }
