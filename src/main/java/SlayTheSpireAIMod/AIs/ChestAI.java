461b49d7dc57497c1433c2d1c949fa97a5482fbe
Ryan Xu <rx26@cornell.edu>
2021-04-02 14:13:47 -0400

    Add don't open chest with Cursed Key

--- src/main/java/SlayTheSpireAIMod/AIs/ChestAI.java
+++ src/main/java/SlayTheSpireAIMod/AIs/ChestAI.java
@@ -1,8 +1,8 @@
 package SlayTheSpireAIMod.AIs;
 
-import SlayTheSpireAIMod.STSAIMod;
 import SlayTheSpireAIMod.communicationmod.ChoiceScreenUtils;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.CursedKey;
 import com.megacrit.cardcrawl.rewards.chests.AbstractChest;
 import com.megacrit.cardcrawl.rooms.TreasureRoom;
 import com.megacrit.cardcrawl.rooms.TreasureRoomBoss;
@@ -13,10 +13,9 @@ import org.apache.logging.log4j.Logger;
 public class ChestAI {
     public static final Logger logger = LogManager.getLogger(ChestAI.class.getName());
 
-    /** Always open the chest, and let the appropriate AI decide what to take. */
+    /** Opens the chest unless player owns relic "Cursed Key". */
     public static void execute(){
         logger.info("Executing...");
-        // TODO
         if(ChoiceScreenUtils.getCurrentChoiceType() != ChoiceScreenUtils.ChoiceType.CHEST){
             logger.info("Done: choice type not suitable");
             return;
@@ -27,18 +26,25 @@ public class ChestAI {
         } else if (AbstractDungeon.getCurrRoom() instanceof TreasureRoom) {
             chest = ((TreasureRoom) AbstractDungeon.getCurrRoom()).chest;
         }
-        assert chest != null;
+        if(chest == null){
+            logger.info("Done: chest does not exist in this room");
+            return;
+        }
+
         if(chest.isOpen){
             // TODO don't press confirm until actions are processed
             if(ChoiceScreenUtils.isConfirmButtonAvailable()){
                 ChoiceScreenUtils.pressConfirmButton();
             }
         }else{
-            logger.info("Opening chest");
-            ChoiceScreenUtils.makeChestRoomChoice(0); //open the chest
+            if(AbstractDungeon.player.hasRelic(CursedKey.ID)){
+                logger.info("Skipping Chest");
+                ChoiceScreenUtils.pressConfirmButton();
+            }else{
+                logger.info("Opening chest");
+                ChoiceScreenUtils.makeChestRoomChoice(0);
+            }
         }
         logger.info("Done");
-
-
     }
 }
