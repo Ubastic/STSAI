4a63a8008e20100a0a84024b743bbee4871ba35a
Ryan Xu <rx26@cornell.edu>
2021-04-09 16:31:21 -0400

    Allow play skills turn 1

--- src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/GremlinNobCAI.java
+++ src/main/java/SlayTheSpireAIMod/AIs/CombatAIs/GremlinNobCAI.java
@@ -1,8 +1,8 @@
 package SlayTheSpireAIMod.AIs.CombatAIs;
 
 import SlayTheSpireAIMod.util.Move;
+import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,26 +15,20 @@ public class GremlinNobCAI extends AbstractCAI {
 
     @Override
     public Move pickMove() {
-        // first, remove cards that cannot be played
         CardSequence start = new CardSequence();
-
-        // never play skills vs Gremlin Nob
-        ArrayList<AbstractCard> skills = new ArrayList<>();
-        for(AbstractCard c : start.simplePlayer.hand){
-            if(c.type == AbstractCard.CardType.SKILL){
-                skills.add(c);
+        if(GameActionManager.turn > 1) {
+            // never play skills to enrage Gremlin Nob
+            ArrayList<AbstractCard> skills = new ArrayList<>();
+            for(AbstractCard c : start.simplePlayer.hand) {
+                if(c.type == AbstractCard.CardType.SKILL) {
+                    skills.add(c);
+                }
+            }
+            for(AbstractCard c : skills) {
+                start.simplePlayer.hand.remove(c);
             }
         }
-        for(AbstractCard c : skills){
-            start.simplePlayer.hand.remove(c);
-        }
 
-        CardSequence bestState = start.getBestPossibility(GenericCAI::heuristic);
-        if(bestState != start){
-            logger.info("Evaluated best state: " + bestState.toString());
-            int bestIndex = AbstractDungeon.player.hand.group.indexOf(bestState.first);
-            return new Move(Move.TYPE.CARD, bestIndex, AbstractDungeon.getCurrRoom().monsters.monsters.get(bestState.firstTargetIndex));
-        }
-        return new Move(Move.TYPE.PASS);
+        return GenericCAI.pickMove(GenericCAI::heuristic, GenericCAI::potionEval, start);
     }
 }
